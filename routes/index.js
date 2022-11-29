60cd1645ca3cc62ee4e7f5ae445053538fdc2085
hctrdubs24 <OtracuentadeHector24@outlook.com>
2022-11-28 20:43:09 -0600

    hola4

--- routes/index.js
+++ routes/index.js
@@ -3,38 +3,46 @@ const nodemailer = require("nodemailer");
 const router = Router();
 
 router.get("/", (req, res) => {
-  res.send("hola");
+  try {
+    res.send("hola");
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 router.post("/email", async (req, res) => {
-  const { email } = req.body;
-  contentHTML = `
+  try {
+    const { email } = req.body;
+    contentHTML = `
     <h2>¡Su cuenta ha sido registrada!</h2>
     <p>Puede ver nuestro catálogo en: <a href="https://copilot-pc.vercel.app/">Copilot PC</a></p>
   `;
-  const transporter = nodemailer.createTransport({
-    // host: "smpt.gmail.com",
-    service: "gmail",
-    port: 465,
-    secure: false,
-    auth: {
-      user: email,
-      pass: "xtbicyokoxruiagb",
-    },
-  });
+    const transporter = nodemailer.createTransport({
+      // host: "smpt.gmail.com",
+      service: "gmail",
+      port: 465,
+      secure: false,
+      auth: {
+        user: email,
+        pass: "xtbicyokoxruiagb",
+      },
+    });
 
-  transporter.verify().then(() => console.log("listo para enviar "));
+    transporter.verify().then(() => console.log("listo para enviar "));
 
-  const info = await transporter.sendMail({
-    from: "Registro de usuario <Copilot PC>",
-    to: "hectordubs24@gmail.com",
-    subject: "Registro de usuario",
-    html: contentHTML,
-  });
+    const info = await transporter.sendMail({
+      from: "Registro de usuario <Copilot PC>",
+      to: "hectordubs24@gmail.com",
+      subject: "Registro de usuario",
+      html: contentHTML,
+    });
 
-  console.log(info);
+    console.log(info);
 
-  res.send("hola");
+    res.send("hola");
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports = router;
